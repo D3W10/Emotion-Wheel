5cf1d223bac8fdf009d45840488fa1609c80ea60
Daniel Nunes <danyribeironunes15@gmail.com>
2024-07-31 18:56:13 +0100

    feat: Add some new spring animations to the emotion wheel

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import Button from "@/components/Button";
 import Icons from "@/components/Icons";
 import IconButton from "@/components/IconButton";
-import { PieChart } from "@mui/x-charts/PieChart";
+import { motion, useSpring } from "framer-motion";
+import { pieArcLabelClasses, PieChart } from "@mui/x-charts/PieChart";
 import { innerData, middleData, outerData } from "@/public/data";
 import type { MakeOptional } from "@mui/x-charts/internals";
 import type { PieSeriesType, PieValueType } from "@mui/x-charts/models/seriesType";
@@ -38,26 +39,58 @@ const series: MakeOptional<PieSeriesType<MakeOptional<PieValueType, "id">>, "typ
     }
 ];
 
+enum Stages {
+    Intro,
+    First,
+    Second,
+    Third,
+    Final
+}
+
+const pieScale: { [key in Stages]: number } = {
+    [Stages.Intro]: 1.6,
+    [Stages.First]: 3,
+    [Stages.Second]: 1,
+    [Stages.Third]: 1,
+    [Stages.Final]: 1.6
+}
+
 export default function Home() {
-    const [isHomeScreen, setHomeScreen] = useState(true);
+    const [stage, setStage] = useState<Stages>(Stages.Intro);
     const [isAnimated, setAnimated] = useState(true);
     const [itemData, setItemData] = useState();
 
+    const springConfig = { damping: 50, stiffness: 100 };
+    const rotateSpring = useSpring(0, springConfig);
+    const scaleSpring = useSpring(0, springConfig);
+
     useEffect(() => {
-        if (isHomeScreen)
+        scaleSpring.set(pieScale[stage])
+
+        if (stage === Stages.Intro)
             setAnimated(true);
-    }, [isHomeScreen]);
+    }, [stage]);
+
+    function onStart() {
+        setAnimated(false);
+        setStage(Stages.First);
+        rotateSpring.set(360 - rotateSpring.get() + 360);
+    }
+
+    function onReset() {
+        setStage(Stages.Intro);
+    }
 
     return (
         <main className="min-h-screen">
-            {isHomeScreen && (
+            {stage === Stages.Intro && (
                 <div className={`h-full p-24 flex flex-col justify-center items-center absolute inset-0 bg-white/75 backdrop-blur space-y-40 transition duration-500 z-10 ${isAnimated ? "ease-out opacity-100 scale-100" : "ease-in opacity-0 scale-125"}`} onTransitionEnd={e => e.propertyName == "opacity" && !isAnimated && setHomeScreen(false)}>
                     <div className="flex flex-col justify-center items-center space-y-10">
                         <Image src="/logo.svg" alt="Logo" width={72} height={72} />
                         <h1 className="text-7xl font-bold">Emotion Wheel</h1>
                     </div>
                     <div className="flex flex-col justify-center items-center space-y-10">
-                        <Button onClick={() => setAnimated(false)}>Start</Button>
+                        <Button onClick={onStart}>Start</Button>
                         <Button secondary>
                             <Icons.CircleQuestion className="w-6 h-6" />
                             <span>How it works</span>
@@ -66,15 +99,23 @@ export default function Home() {
                 </div>
             )}
             <div className="h-full flex justify-center items-center absolute inset-0">
-                <div className="h-5/6 aspect-square">
+                <motion.div className="h-full aspect-square" style={{ rotate: rotateSpring, scale: scaleSpring }}>
                     <PieChart
                         series={series}
                         tooltip={{ trigger: "none" }}
+                        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
                         slotProps={{ legend: { hidden: true } }}
-                        onItemClick={(event, d) => setItemData(d)}
+                        onItemClick={(_, d) => { console.log(d);setItemData(d)}}
+                        sx={{
+                            [`& .${pieArcLabelClasses.root}`]: {
+                                fontSize: "0.7rem",
+                                fill: "white",
+                                filter: "drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))"
+                            },
+                        }}
                     />
-                </div>
-                <IconButton className="absolute bottom-6 left-6" onClick={() => setHomeScreen(true)}>
+                </motion.div>
+                <IconButton className="absolute bottom-6 left-6" onClick={onReset}>
                     <Icons.Home className="w-8 h-8" />
                 </IconButton>
             </div>
