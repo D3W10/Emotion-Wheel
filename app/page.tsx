bff66f154a7430832605de1e67a56d79f3feebf1
Daniel Nunes <danyribeironunes15@gmail.com>
2024-07-31 12:05:47 +0100

    feat: Add button to return to the home screen along with new transition animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Button from "@/components/Button";
 import Icons from "@/components/Icons";
+import IconButton from "@/components/IconButton";
 import { PieChart } from "@mui/x-charts/PieChart";
 import { innerData, middleData, outerData } from "@/public/data";
 import type { MakeOptional } from "@mui/x-charts/internals";
@@ -38,19 +39,25 @@ const series: MakeOptional<PieSeriesType<MakeOptional<PieValueType, "id">>, "typ
 ];
 
 export default function Home() {
-    const [homeScreen, setHomeScreen] = useState(true);
+    const [isHomeScreen, setHomeScreen] = useState(true);
+    const [isAnimated, setAnimated] = useState(true);
     const [itemData, setItemData] = useState();
+
+    useEffect(() => {
+        if (isHomeScreen)
+            setAnimated(true);
+    }, [isHomeScreen]);
 
     return (
         <main className="min-h-screen">
-            {homeScreen && (
-                <div className="h-full p-24 flex flex-col justify-center items-center absolute inset-0 bg-white/75 backdrop-blur space-y-40 z-10">
+            {isHomeScreen && (
+                <div className={`h-full p-24 flex flex-col justify-center items-center absolute inset-0 bg-white/75 backdrop-blur space-y-40 transition duration-500 z-10 ${isAnimated ? "ease-out opacity-100 scale-100" : "ease-in opacity-0 scale-125"}`} onTransitionEnd={e => e.propertyName == "opacity" && !isAnimated && setHomeScreen(false)}>
                     <div className="flex flex-col justify-center items-center space-y-10">
                         <Image src="/logo.svg" alt="Logo" width={72} height={72} />
                         <h1 className="text-7xl font-bold">Emotion Wheel</h1>
                     </div>
                     <div className="flex flex-col justify-center items-center space-y-10">
-                        <Button onClick={() => setHomeScreen(false)}>Start</Button>
+                        <Button onClick={() => setAnimated(false)}>Start</Button>
                         <Button secondary>
                             <Icons.CircleQuestion className="w-6 h-6" />
                             <span>How it works</span>
@@ -67,6 +74,9 @@ export default function Home() {
                         onItemClick={(event, d) => setItemData(d)}
                     />
                 </div>
+                <IconButton className="absolute bottom-6 left-6" onClick={() => setHomeScreen(true)}>
+                    <Icons.Home className="w-8 h-8" />
+                </IconButton>
             </div>
         </main>
     )
